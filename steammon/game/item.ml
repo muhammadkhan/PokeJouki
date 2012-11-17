416eddef08efd113bdf38fcfdef680d23c6c1517
aab227 <arjunbiddanda@gmail.com>
2012-11-17 16:49:43 -0500

    added item module, assumed that the type we will use is refs; we need to define the mutable game type though

---
+++ steammon/game/item.ml
@@ -0,0 +1,62 @@
+
+open Definitions
+(**
+This is the module that controls all of the item usage 
+And their subsequent effects on steammon
+Should the state module be used here? 
+*)
+
+(*Do we create a separate module that handles all of the effects?*)
+
+(*At this point we are assuming that all of the fields are mutable*)
+
+(*using one of the X stat-boosting items*)
+(*NOT COMPLETED*)
+let use_X_item (i : item) : unit = 	()
+
+
+(*Using an ether on a pokemon*)		
+let use_Ether (i : item) (p: steammon) : unit = 
+	if (!(List.nth inventory 0) <> 0) then
+	 (p.first_attack.pp_remaining := !(p.first_attack.pp_remaining) + 5;
+		p.second_attack.pp_remaining := !(p.second_attack.pp_remaining) + 5;
+		p.third_attack.pp_remaining := !(p.third_attack.pp_remaining) + 5;
+		p.fourth_attack.pp_remaining := !(p.fourth_attack.pp_remaining) + 5;
+		List.nth inventory 0 := !(List.nth inventory 0) - 1;	
+		)
+	else ()	
+
+(*using a maxPotion on a pokemon*)	
+let use_maxPotion (i : item) (p : steammon) : unit =	
+	(*first check if we have some to use*)
+	if (!(List.nth inventory 1) <> 0) then
+		(if (!p.curr_hp <> 0) then	 
+	    (p.curr_hp := p.max_hp; 
+	    (List.nth inventory 2) := !(List.nth inventory 2) - 1;
+		  )
+		 else ()	
+		)
+	else ()
+
+			
+						
+(*Using a revive on a fainted pokemon*)												
+let use_Revive (i : item) (p : steammon) : unit = 
+  if (!(List.nth inventory 2) <> 0) then
+		(if !p.curr_hp = 0 then
+			(p.curr_hp := p.max_hp / 2;
+			 List.nth inventory 2 := !(List.nth inventory 2) - 1; 
+			)
+		 else ();	
+		)
+	else ()		
+
+
+
+
+
+
+
+										
+		
+				
