63e23cd37fc3b12ba30154cd506d5612c18b7329
aab227 <arjunbiddanda@gmail.com>
2012-11-18 15:41:46 -0500

    revised item module to current game standards

--- steammon/game/item.ml
+++ steammon/game/item.ml
@@ -1,67 +1,109 @@
-
 open Definitions
+open State
+
+
+
 (**
 This is the module that controls all of the item usage 
 And their subsequent effects on steammon
 Should the state module be used here? 
 *)
 
-(*Do we create a separate module that handles all of the effects?*)
 
-(*At this point we are assuming that all of the fields are mutable*)
+
+
 
 (*Using an ether on a pokemon*)		
-let use_Ether (i : item) (p: steammon) : unit = 
-	if (!(List.nth inventory 0) <> 0) then
-	 (p.first_attack.pp_remaining := !(p.first_attack.pp_remaining) + 5;
-		p.second_attack.pp_remaining := !(p.second_attack.pp_remaining) + 5;
-		p.third_attack.pp_remaining := !(p.third_attack.pp_remaining) + 5;
-		p.fourth_attack.pp_remaining := !(p.fourth_attack.pp_remaining) + 5;
-		List.nth inventory 0 := !(List.nth inventory 0) - 1;	
-		)
-	else ()	
+let use_Ether (p: steammon ref) : unit = 
+	p :=  
+		{species = (!p).species; 
+		 curr_hp = (!p).curr_hp;
+		 max_hp = (!p).max_hp; 
+		 first_type = (!p).first_type;
+		 second_type = (!p).second_type; 
+		 first_attack = change_pp_by ((!p).first_attack) (5);
+		 second_attack = change_pp_by ((!p).second_attack) (5);
+		 third_attack = change_pp_by ((!p).third_attack) (5);
+		 fourth_attack = change_pp_by ((!p).fourth_attack) (5);
+		 attack = (!p).attack;
+		 spl_attack = (!p).spl_attack;
+		 defense = (!p).defense;
+		 spl_defense = (!p).spl_defense;
+		 speed = (!p).speed;
+		 status = (!p).status;
+		 mods = (!p).mods
+		} 
 
 (*using a maxPotion on a pokemon*)	
-let use_maxPotion (i : item) (p : steammon) : unit =	
-	(*first check if we have some to use*)
-	if (!(List.nth inventory 1) <> 0) then
-		(if (!p.curr_hp <> 0) then	 
-	    (p.curr_hp := p.max_hp; 
-	    (List.nth inventory 2) := !(List.nth inventory 2) - 1;
-		  )
-		 else ()	
-		)
-	else ()
-						
+let use_maxPotion (p : steammon ref) : unit =
+  p := 
+		{species = (!p).species; 
+		 curr_hp = (!p).max_hp;
+		 max_hp = (!p).max_hp; 
+		 first_type = (!p).first_type;
+		 second_type = (!p).second_type; 
+		 first_attack = (!p).first_attack;
+		 second_attack = (!p).second_attack;
+		 third_attack = (!p).third_attack;
+		 fourth_attack =  (!p).fourth_attack;
+		 attack = (!p).attack;
+		 spl_attack = (!p).spl_attack;
+		 defense = (!p).defense;
+		 spl_defense = (!p).spl_defense;
+		 speed = (!p).speed;
+		 status = (!p).status;
+		 mods = (!p).mods
+		}
+
 (*Using a revive on a fainted pokemon*)												
-let use_Revive (i : item) (p : steammon) : unit = 
-  if (!(List.nth inventory 2) <> 0) then
-		(if !p.curr_hp = 0 then
-			(p.curr_hp := p.max_hp / 2;
-			 List.nth inventory 2 := !(List.nth inventory 2) - 1; 
-			)
-		 else ();	
-		)
-	else ()
+let use_Revive (p : steammon ref) : unit =
+  if ((!p).curr_hp = 0) then
+		p := 
+			{species = (!p).species; 
+  		 curr_hp = ((!p).max_hp / 2); (*We revive to half-health *)
+  		 max_hp = (!p).max_hp; 
+  		 first_type = (!p).first_type;
+  		 second_type = (!p).second_type; 
+  		 first_attack = (!p).first_attack;
+  		 second_attack = (!p).second_attack;
+  		 third_attack = (!p).third_attack;
+  		 fourth_attack =  (!p).fourth_attack;
+  		 attack = (!p).attack;
+  		 spl_attack = (!p).spl_attack;
+  		 defense = (!p).defense;
+  		 spl_defense = (!p).spl_defense;
+  		 speed = (!p).speed;
+  		 status = (!p).status;
+  		 mods = (!p).mods
+			}
+  else ()
 	
 (*Using a full heal on a pokemon*)			
-let use_FullHeal (i : item) (p: steammon) : unit = 
-	if (!(List.nth inventory 3) <> 0) then
-		(p.status := []; 
-		 List.nth inventory 3 := !(List.nth inventory 3) - 1;
-		)
-	else ()					
-
+let use_FullHeal (p: steammon ref) : unit =
+	p := 
+		{species = (!p).species; 
+		 curr_hp = (!p).max_hp;
+		 max_hp = (!p).max_hp; 
+		 first_type = (!p).first_type;
+		 second_type = (!p).second_type; 
+		 first_attack = (!p).first_attack;
+		 second_attack = (!p).second_attack;
+		 third_attack = (!p).third_attack;
+		 fourth_attack =  (!p).fourth_attack;
+		 attack = (!p).attack;
+		 spl_attack = (!p).spl_attack;
+		 defense = (!p).defense;
+		 spl_defense = (!p).spl_defense;
+		 speed = (!p).speed;
+		 status = [];
+		 mods = (!p).mods
+		}
+		
 (*using one of the X stat-boosting items*)
 (*NOT COMPLETED*)
 (*This should just increment the specific modifier*)
 let use_X_item (i : item) : unit = 	()
 
-
-
-
-
-
-										
+						
 		
 				
