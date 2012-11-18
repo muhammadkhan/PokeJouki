26a6766cb9968dc408b61b108b0c05b79fcec236
Muhammad Khan <mhk98@cornell.edu>
2012-11-18 04:13:05 -0500

    worked on game.ml

--- steammon/game/game.ml
+++ steammon/game/game.ml
@@ -5,20 +5,53 @@ open Netgraphics
 
 type team = {
 	id: color;
-	smons: (steammon * int ref) list;
+	steammons: steammon ref list;
 	items: item list
 }
 
 (* You have to implement this. Change it from int to yout own state type*)
+
+(*first team = red, second team = blue*)
 type game = team * team
 
-let game_datafication g =
-	g.teams
-	
-let game_from_data game_data = 
-	failwith "not implemented"
+let game_datafication g : game_status_data =
+	let (redTeam, blueTeam) = g in
+	let item_to_int i =
+		match i with
+			| Ether -> 1
+			| MaxPotion -> 2
+			| Revive -> 3
+			| FullHeal -> 4
+			| XAttack -> 5
+			| XDefense -> 6
+			| XSpeed -> 7
+			| XAccuracy -> 8
+	in
+	((List.map (fun s -> !s) redTeam.steammons, List.map item_to_int redTeam.items),
+	(List.map (fun s -> !s) blueTeam.steammons, List.map item_to_int blueTeam.items))
 
-let handle_step g ra ba =
+let game_from_data game_data : game = 
+	let int_to_item n =
+		match n with
+			| 1 -> Ether
+			| 2 -> MaxPotion
+			| 3 -> Revive
+			| 4 -> FullHeal
+			| 5 -> XAttack
+			| 6 -> XDefense
+			| 7 -> XSpeed
+			| 8 -> XAccuracy
+			| _ -> failwith "no item corresponds to this index"
+	in
+	let ((r_slist, r_inv), (b_slist, b_inv)) = game_data in
+	let red_team = {id = Red; steammons = List.map (fun s -> ref s) r_slist;
+	    items = List.map int_to_item r_inv} in
+	let blue_team = {id = Blue; steammons = List.map (fun s -> ref s) b_slist;
+	    items = List.map int_to_item b_inv} in
+	(red_team, blue_team)
+
+let handle_step g ra ba : game_output =
+	let (r_old, b_old) = g in
 	failwith "implement me!"
 
 let init_game () =
--- steammon/game/state.ml
+++ steammon/game/state.ml
@@ -6,6 +6,16 @@ open Attack
 (*of properties like HP, PP, inventory, etc*)
 
 let get_hp (p : steammon) = p.curr_hp
+
+let change_hp_by (p : steammmon) (delta : int) : steammon = {
+	species = p.species; curr_hp = p.curr_hp + delta; max_hp = p.max_hp;
+	first_type = p.first_type; second_type = p.second_type;
+	first_attack = p.first_attack; second_attack = p.second_attack;
+	third_attack = p.third_attack; fourth_attack = p.fourth_attack;
+	attack = p.attack; spl_attack = p.spl_attack; defense = p.defense;
+	spl_defense = p.spl_defense; speed = p.speed; status = p.status;
+	mods = p.mods
+}
 	
 let get_pp (a : attack) = a.pp_remaining
 	
@@ -14,3 +24,8 @@ let change_pp_by (a : attack) (delta : int) : attack = {
 	pp_remaining = a.pp_remaining + delta; power = a.power; accuracy = a.accuracy;
 	crit_chance = a.crit_chance; effect = a.effect
 }
+
+let rec all_are_dead (sl : steammon list) : bool =
+	match sl with
+		| [] -> true
+		| h::t -> h.curr_hp = 0 && all_are_dead t
